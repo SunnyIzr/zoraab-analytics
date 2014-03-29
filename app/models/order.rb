fa615b4ca5ac34df7e2e83b7fc58dfc35988c51f
Sunny Israni <sunnyisrani@Sunnys-MacBook-Pro.local>
2014-03-29 00:06:25 -0400

    creating a method to save shopify order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
-  validates_uniqueness_of :order_number
-  validates_presence_of :order_number
+  validates_uniqueness_of :number
+  validates_presence_of :number
+  
+  def self.shopify(order)
+    new_order = self.new(created_at: order[:created_at], number: order[:number], total: order[:total])
+    new_order.customer = Customer.find_or_create_by(email: order[:email], name: order[:billing_address][:name])
+    new_order.save
+  end
 end
